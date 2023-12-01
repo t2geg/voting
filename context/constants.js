b60b9bfb00ece84457967f7877d5e6f0b71e9f5a
Ashish Kumar Gupta <87427147+t2geg@users.noreply.github.com>
2023-12-01 22:13:07 +0530

    done

--- context/constants.js
+++ context/constants.js
@@ -2,5 +2,6 @@
 
 import voting from "./Create.json";
 
-export const VotingAddress = '0x2286e1947d1Ef63F78abFcc55a4C497152657670';
+// export const VotingAddress = '0x2286e1947d1Ef63F78abFcc55a4C497152657670';  // for deployed
+export const VotingAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 export const VotingAddressABI = voting.abi;
